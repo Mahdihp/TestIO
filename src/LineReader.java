c613ac9c584bc46f7e076049fb7a7527f2882841
mahdi <mahdihp.devsc@gmail.com>
2018-09-05 16:49:06 +0430

    Java IO

--- src/LineReader.java
+++ src/LineReader.java
@@ -2,6 +2,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Scanner;
 
 public class LineReader {
 
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,44 @@
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        LineReader lineReader = new LineReader(
-                "/home/mahdi/IdeaProjects/TestIO/readme.txt",
-                "/home/mahdi/IdeaProjects/TestIO/readme2.txt");
-//        lineReader.showReverseLine();
-        lineReader.showReversLine2();
+//        LineReader lineReader = new LineReader(
+//                "/home/mahdi/IdeaProjects/TestIO/readme.txt",
+//                "/home/mahdi/IdeaProjects/TestIO/readme2.txt");
+////        lineReader.showReverseLine();
+//        lineReader.showReversLine2();
+        String path = "/home/mahdi/IdeaProjects";
+        File file = new File(path);
+
+        double result = ((folderSize(file) / 1024) / 1024);
+        System.out.println(result + "MB");
+
     }
+
+
+
+
+    public static long folderSize(File directory) {
+        long length = 0;
+        for (File file : directory.listFiles()) {
+            if (file.isFile())
+                length += file.length();
+            else
+                length += folderSize(file);
+        }
+        return length;
+    }
+
+
+
 }
